80f6eea45d2d29410c0884213cfc09e56c84def6
Quy Nguyen <nguyenngocquy020196@gmail.com>
2022-10-10 20:47:49 +0700

    Update CloudflareServiceProvider.php

--- src/Providers/CloudflareServiceProvider.php
+++ src/Providers/CloudflareServiceProvider.php
@@ -12,22 +12,75 @@ class CloudflareServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../../config/cloudflare.php', 'cloudflare');
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'cloudflare');
+        $this->registerCommands();
+        $this->registerPublishing();
 
-        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
-        $this->loadRoutesFrom(__DIR__ . '/../../routes/api.php');
-
-        if ($this->app->runningInConsole()) {
-            $this->publishes([__DIR__ . '/../../config/cloudflare.php' => config_path('cloudflare.php')], 'config');
-            $this->publishes([__DIR__ . '/../../resources/views' => resource_path('views/vendor/assets')], 'views');
+        if (!config('cloudflare.enabled')) {
+            return;
         }
+       
+        Route::middlewareGroup('cloudflare', config('cloudflare.middleware', []));
+        Route::middlewareGroup('cloudflare:api', [
+            ForceJsonResponseMiddleware::class
+        ]));
+        
+        $this->registerRoutes();
+        $this->registerMigrations();
+        
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'cloudflare');
     }
 
     public function register()
     {
         File::requireOnce(__DIR__ . '/../../helpers/common.php');
-        $router = $this->app['router'];
-        $router->aliasMiddleware('cloudflare:api', ForceJsonResponseMiddleware::class);
+        $this->mergeConfigFrom(__DIR__ . '/../../config/cloudflare.php', 'cloudflare');
+    }
+    
+    private function registerRoutes()
+    {
+        Route::group($this->routeConfiguration(), function () {
+            $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
+        });
+        Route::group($this->routeConfiguration(true), function () {
+            $this->loadRoutesFrom(__DIR__ . '/../../routes/api.php');
+        });
+    }
+    
+    private function routeConfiguration($api = false)
+    {
+        $routeConfig = [
+            'domain' => config('cloudflare.domain', null),
+            'namespace' => 'Rovahub\Cloudflare\Http\Controllers',
+            'prefix' => config('cloudflare.path'),
+        ];
+        if(!$api){
+           return array_merge($routeConfig, ['middleware' => 'cloudflare']);
+        }
+        return array_merge($routeConfig, ['middleware' => 'cloudflare:api']);
+    }
+    
+    private function registerPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([__DIR__.'/../database/migrations' => database_path('migrations')], 'migrations');
+            $this->publishes([__DIR__ . '/../../config/cloudflare.php' => config_path('cloudflare.php')], 'config');
+            $this->publishes([__DIR__ . '/../../resources/views' => resource_path('views/vendor/assets')], 'views');
+        }
+    }
+    
+    private function registerMigrations()
+    {
+        if ($this->app->runningInConsole() {
+            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        }
+    }
+    
+    protected function registerCommands()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+               
+            ]);
+        }
     }
 }
