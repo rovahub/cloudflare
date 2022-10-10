3c4b2644f8ebc489571243bf0cc643559272f507
Quy Nguyen <nguyenngocquy020196@gmail.com>
2022-10-10 20:05:41 +0700

    Update cloudflare.php

--- config/cloudflare.php
+++ config/cloudflare.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'api_key' => env('CLOUDFLARE_API', null),
+    'token' => env('CLOUDFLARE_TOKEN', null),
     'domain' => env('CLOUDFLARE_DOMAIN', null),
     'path' => env('CLOUDFLARE_PATH', 'cloudflare'),
     'key' => env('CLOUDFLARE_KEY', null)
