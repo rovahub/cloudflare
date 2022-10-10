59c47195ee888c8ed1f938fab31abf58312e5bbf
Quy Nguyen <nguyenngocquy020196@gmail.com>
2022-10-10 19:22:07 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(array_merge(['middleware' => 'cloudflare-api'], cloudflare_route_group('api')),
+Route::group(array_merge(['middleware' => 'cloudflare:api'], cloudflare_route_group('api')),
     function () {
         Route::get('test', function (\Rovahub\Cloudflare\Http\Responses\BaseHttpResponse $response) {
             return $response->setMessage('asjdhaksd');
