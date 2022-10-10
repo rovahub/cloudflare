9e0b5477d51e94c0e3f16e3292c60e62785f821d
Quy Nguyen <nguyenngocquy020196@gmail.com>
2022-10-10 18:49:30 +0700

    [FIX] config helper

--- helpers/common.php
+++ helpers/common.php
@@ -3,7 +3,7 @@
 if (!function_exists('cloudflare_prefix')) {
     function cloudflare_prefix($type = null)
     {
-        $path = config('cloudflare.cloudflare_panel_path');
+        $path = config('cloudflare.panel_path');
         return $path . ($type === 'api' ? '/api' : '');
     }
 
@@ -14,7 +14,7 @@ if (!function_exists('cloudflare_route_group')) {
     {
         $domain = config('cloudflare.cloudflare_panel_domain');
         $prefix = cloudflare_prefix($type);
-        if (config('cloudflare.cloudflare_panel_domain')) {
+        if ($domain) {
             return [
                 'domain' => $domain,
                 'prefix' => $prefix
