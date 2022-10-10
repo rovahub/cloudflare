812ec8a113c7b243f39c424c30bb4f5b4f4ac17e
Quy Nguyen <nguyenngocquy020196@gmail.com>
2022-10-10 20:49:21 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Rovahub\Cloudflare\Http\Controllers\AuthController;
 
-Route::group(cloudflare_route_group(), function () {
-    Route::group(['prefix' => 'auth'], function () {
-        Route::get('signin', [AuthController::class, 'signin'])->name('cloudflare:signin');
-    });
+Route::group(['prefix' => 'auth'], function () {
+    Route::get('signin', [AuthController::class, 'signin'])->name('cloudflare:signin');
 });
